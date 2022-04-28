b647871aa7ad24d2e185e92e082b15a344a86c67
Crown003 <80181799+Crown003@users.noreply.github.com>
2022-04-28 07:39:11 +0530

    Updated new functions
    Done

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ Role_of_user = ""
 def start():
 	global Role_of_user
 	Role_of_user = User_role_provider(input("Enter your role i.e(teacher/student): "),input("Enter your password: "))
-
-start()
-chance_left = 3
 def submit_data(Role_of_user):
 	Task = ""
 	if Role_of_user == "Teacher":
-		print("\n" + Fore.YELLOW + "Namaste sir/mam ! Please choose your task: ")
-		print("""Type a for - Add a new student\n\tu for - Update Student details\n\tr for - Remove Student\n\tg for - Get student Details\n""" )
-		print("""Type 'q' to exit.""")
+		print("\n" + Fore.YELLOW + "Namaste sir/mam !")
 		while True:
-			Task = input("》》》》")
+			print(Fore.GREEN+ "Please choose your task: ")
+			print(Fore.GREEN+"""Type a for - Add a new student\n\tu for - Update Student details\n\tr for - Remove Student\n\tg for - Get student Details\n""" )
+			print(Fore.BLUE+"""Type 'q' to exit.""")
+			Task = input(Fore.GREEN+"》》》》")
 			if Task.upper() == "A":
-				print("Please input student details.\n")
+				print(Fore.GREEN + "Please input student details.\n")
 				std_name = str(input("Enter the student Name: "))
 				std_class = str(input("Enter class + section: "))
 				std_rollno = str(input("Enter roll number: "))
@@ -36,7 +34,7 @@ def submit_data(Role_of_user):
 						
 				student_details = std_rollno+" "+std_name+" "+std_class+" "+mrk_of_std_Unit_1+" "+std_stream
 				print(Fore.BLUE + f"""\n\nNAME: {std_name}\nClass: {std_class}\nRoll no: {std_rollno}\nStream: {std_stream}\n Marks of unit 1st Test:\n {mrk_of_std_Unit_1}""")
-				a = input(Fore.YELLOW+ "Please check the details. And type 's' to save and 'c' to cancel.\n")
+				a = input(Fore.YELLOW+ "Please check the details. And type 's' to save and 'c' to cancel.\n\n")
 				if a.upper() == "S":
 					add_student(Role_of_user,Student_details = student_details)
 					print(Fore.GREEN+"Data Added data successfully.")
@@ -44,20 +42,20 @@ def submit_data(Role_of_user):
 					continue;
 				else:
 					print("invalid input.")
-				last_decision = input("Enter 'menu' to go back to menu  and 'q' to quit.")
+				last_decision = input(Fore.YELLOW + "Enter 'menu' to go back to menu  and 'q' to quit.")
 				for i in range(2):
 					if last_decision.upper() == "MORE":
 						submit_data("Teacher")
 					elif last_decision.upper() == "Q":
 						break
 					else:
-						print("Enter valid option.")
+						print(Fore.RED + "Enter valid option.")
 						if i<2:
 							continue
 						else:
 							break
 			elif Task.upper() == "U":
-				pass
+				update_student_details()
 				break;
 			elif Task.upper() == "R":
 				pass
@@ -68,11 +66,24 @@ def submit_data(Role_of_user):
 				break;
 			else:
 				chance_left -= 1
-				print(f"Invalid input. Please choose the correct value. You have only {chance_left} try left.")
+				print(Fore.RED + f"Invalid input. Please choose the correct value. You have only {chance_left} try left.")
 				continue
 	elif Role_of_user == "Student":
-		print("Hello ! Please choose your task: ")
-		print("""Type P for - Profile view R for - Result\n""" )
-		Task = input("=> ")
+		print(Fore.YELLOW +"\n\nHello ! Student \n")
+		while True:
+			print(Fore.GREEN + "Please choose your task: ")
+			print(Fore.GREEN+ """Type P for - Profile view R for - Result\n""" )
+			print(Fore.BLUE+"Type 'q' to quit.")
+			Task = input(Fore.GREEN + "=> ")
+			if Task.upper() == "P":
+				Show_profile()
+			elif Task.upper() == "R":
+				pass
+				#show_student_Result()
+			elif Task.upper() == "Q":
+				pass
+			else:
+				print(Fore.RED + "Enter some valid input.")
 
+start()
 submit_data(Role_of_user)
